6bdd1547f2c139c52fedae0060ae0c4c62d96310
iimmonica <iimmonica@outlook.com>
2017-01-12 18:20:28 +0800

    first chart

---
+++ js/chart.js
@@ -0,0 +1,56 @@
+function loadViewChart() {
+	$(function() {
+		myChart = echarts.init(document.getElementById('tcpViewBox'));
+		defaultOption = {
+			title: {
+				text: "标题",
+				textAlign: 'center',
+				left: 'center'
+			},
+			color: [
+				'#66CCFF', '#9999CC', '#66CC33', '#FFCC33', '#FF6666', '#339999', '#6699FF', '#CC99FF',
+				'#99CC99', '#CCCC66', '#CC9999', '#99CCCC', '#96DCFF', '#B9B9DC', '#85D65D', '#FFD866',
+				'#FF9696', '#40BFBF', '#96B9FF', '#E2C5FF', '#B9DCB9', '#D8D88C', '#DCB9B9', '#B9DCDC',
+			]
+		};
+		option = {
+			title: {
+				text: "2016年度热词",
+				textAlign: 'center', 
+				left: 'center',
+				textStyle: {
+					fontWeight: 'normal'
+				}
+			},
+			series: [{
+				type: 'wordCloud',
+				gridSize: 50,
+				sizeRange: [12, 50],
+				rotationRange: [0, 0],
+				shape: 'rect',
+				textStyle: {
+					normal: {
+						color: function(params) {
+							 return defaultOption.color[params.dataIndex%24];
+						}
+					},
+					emphasis: {
+						shadowBlur: 10,
+						shadowColor: 'rgba(13, 19, 24, 0.2)'
+					}
+				},
+				data: []
+			}]
+		};
+		$.getJSON("data/hotwordsof2016.json", function(data){
+			for(var i = 0; i <　data.length; i++){
+				var d = {
+					name:　data[i][0], 
+					value: data[i][1]
+				};
+				option.series[0].data.push(d);
+			}
+			myChart.setOption(option);
+		});
+	});
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
  $(document).ready(function() {
- 	$(".showmore").on("click", function(){
- 		$(".p-tcp-line[tcp="+$(this).attr("tcp")+"]").append($('<div class="p-tcp"></div>'));
- 	});
+ 	$(".body-page").hide();
+ 	$("#chartView").show();
+ 	loadViewChart();
  });
